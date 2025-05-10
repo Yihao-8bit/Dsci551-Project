dcd18d939e118943b10d252e2cbb5f9bd6621214
Yihao Wang <yh82620027@gmail.com>
2025-05-09 20:11:29 -0700

    New Update by wyh

--- dsci551_project/dsci551/views.py
+++ dsci551_project/dsci551/views.py
@@ -26,11 +26,10 @@ def connect_database(request):
     if request.method == 'POST':
         host = request.POST.get('host')
         username = request.POST.get('username')
-        password = request.POST.get('password')  # 允许密码为空
+        password = request.POST.get('password')  
         dbname = request.POST.get('dbname')
 
         try:
-            # 尝试连接到用户提供的数据库
             connection = pymysql.connect(
                 host=host,
                 user=username,
@@ -38,7 +37,6 @@ def connect_database(request):
                 database=dbname
             )
 
-            # 将数据库连接信息保存到 session 中
             request.session['db_connection'] = {
                 'host': host,
                 'user': username,
@@ -46,13 +44,13 @@ def connect_database(request):
                 'database': dbname
             }
 
-            connection.close()  # 连接成功后立即关闭连接
+            connection.close()
             return JsonResponse({'success': True})
 
         except pymysql.MySQLError as e:
-            return JsonResponse({'error': f"数据库连接失败: {str(e)}"}, status=500)
+            return JsonResponse({'error': f"Database connect error: {str(e)}"}, status=500)
 
-    return JsonResponse({'error': '无效请求'}, status=400)
+    return JsonResponse({'error': 'Invalid request'}, status=400)
 
 
 @login_required
@@ -184,13 +182,11 @@ def nosql_mongo_query(request):
         # 打印清洗后的 SQL 语句（如果需要查看清洗后的 SQL） 
         print(f"清洗后的 SQL 查询语句：\n{safe_code}")
         try:
-            # 执行 MySQL 代码
-            #client = MongoClient("mongodb://localhost:27017/")
+
             client = MongoClient(uri)
             db = client[db_name]
             safe_locals = {'db': db,"result": ""}
             exec(safe_code, {}, safe_locals)
-            # 存储用户查询和 LLM 返回的结果
             query_history = UserQueryHistory(user=request.user, query_text=user_input, llm_response=str(safe_locals['result']))
             query_history.save()
             my_output = safe_locals['result']
@@ -209,7 +205,6 @@ def nosql_mongo_query(request):
                 my_output = f"Update Success! Matched {my_output.matched_count}, modified {my_output.modified_count}."
 
 
-            # 返回查询结果
             if isinstance(my_output, list):
                 return JsonResponse({"result": my_output})
             else:
@@ -240,7 +235,7 @@ def select_database(request):
 
 def get_db_schema():
     schema = []
-    for model in [Customer, Order, Product]:  # 列出你的所有模型
+    for model in [Customer, Order, Product]: 
         table_name = model._meta.db_table
         columns = [field.name for field in model._meta.fields]
         schema.append(f"Table: {table_name}\nColumns:\n" + "\n".join([f"  {col}: {get_column_type(model, col)}" for col in columns]))
@@ -248,19 +243,16 @@ def get_db_schema():
 
 def get_column_type(model, column_name):
     field = model._meta.get_field(column_name)
-    return str(field.get_internal_type())  # 获取字段类型
+    return str(field.get_internal_type()) 
 
 
-# 仅限登录用户查看自己的查询历史
 @login_required
 def user_query_history(request):
-    # 获取当前登录用户的查询历史
     query_history = UserQueryHistory.objects.filter(user=request.user).order_by('-timestamp')
 
     return render(request, 'query_history.html', {'query_history': query_history})
 
 
-# 主页选择页面
 def home(request):
     popup_message = request.session.pop('popup_message', None)
     return render(request, 'home.html', {"popup_message": popup_message})
@@ -302,7 +294,6 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # 登录成功后，重定向到选择数据库页面
             return redirect('select_database')  
         else:
             request.session['popup_message'] = 'The username or password is incorrect!'
@@ -340,23 +331,20 @@ def call_deepseek(messages, deepseek_api_key: str, deepseek_api_url: str, model=
         content = message_dict.get("content")
         return reasoning, content
     except (KeyError, IndexError):
-        print("[DeepSeek] return format不符合预期:", data)
+        print("[DeepSeek] return format is not expected:", data)
         return None, None
 
-# 清洗代码块
 def clean_code_block(code_text: str) -> str:
-    # 删除代码块标记
-    code_text = code_text.replace("ok. I'm ready.", "").strip()  # 去除 "ok" 以及多余空格
+    code_text = code_text.replace("ok. I'm ready.", "").strip() 
     code_text = re.sub(r"```python\s*", "", code_text)
     code_text = re.sub(r"```Python\s*", "", code_text)
     code_text = re.sub(r"```", "", code_text)
     code_text = re.sub(r"^\s*sql\s*", "", code_text)
-    # 删除可能出现的 'sql' 字符串
     code_text = re.sub(r"sql", "", code_text)
 
     code_text = re.sub(r"```sql", "", code_text)
 
-    code_text = code_text.strip()  # 去除两端的空格
+    code_text = code_text.strip() 
 
     return code_text
 
@@ -364,17 +352,14 @@ def clean_code_block(code_text: str) -> str:
 def get_db_schema(db_connection):
     schema = []
     try:
-        # 获取数据库连接
         connection = pymysql.connect(**db_connection)
         cursor = connection.cursor()
 
-        # 获取所有表的名称
         cursor.execute("SHOW TABLES")
         tables = cursor.fetchall()
 
         for table in tables:
             table_name = table[0]
-            # 获取每个表的列信息
             cursor.execute(f"DESCRIBE {table_name}")
             columns = cursor.fetchall()
 
@@ -391,8 +376,6 @@ def get_db_schema(db_connection):
 
     return "\n\n".join(schema)
 
-
-# 仅登录用户可以进行查询
 @login_required
 def natural_language_query(request):
     if request.method == 'POST':
@@ -400,18 +383,15 @@ def natural_language_query(request):
         deepseek_api_key = "sk-c6d9cd93a21b418da5adc6ef7fcb2479"
         deepseek_api_url = "https://api.deepseek.com/v1/chat/completions"
 
-        db_connection = request.session.get('db_connection')  # 从 session 中获取数据库连接信息
+        db_connection = request.session.get('db_connection')
         
         if db_connection is None:
-            return JsonResponse({"error": "未连接到数据库，请先提供数据库连接信息。"})
+            return JsonResponse({"error": "Can not connect to database"})
 
-        # 动态获取数据库 schema
         db_schema = get_db_schema(db_connection)
         
         if db_schema is None:
-            return JsonResponse({"error": "无法获取数据库 schema。"})
-
-        # LLM 提示词
+            return JsonResponse({"error": "Can not get database schema。"})
         MYSQL_SYSTEM_INSTRUCTION = (
             f"You are my MySQL database assistant. Here is the database schema:\n{db_schema}\n"
             "I will give you natural language commands related to exploring the schema, querying, inserting, updating, and deleting. "
@@ -426,28 +406,22 @@ def natural_language_query(request):
         messages = [{"role": "system", "content": MYSQL_SYSTEM_INSTRUCTION}]
         messages.append({"role": "user", "content": user_input})
 
-        # 调用 DeepSeek API
         reasoning, content = call_deepseek(messages, deepseek_api_key, deepseek_api_url)
         if not content:
             return JsonResponse({"error": "Error processing your request."}, status=400)
 
-        # 打印 LLM 返回的 SQL 语句
-        print(f"LLM 返回的 SQL 查询语句：\n{content}")
+        print(f"SQL returned by LLM:\n{content}")
 
-        # 清洗并执行返回的代码
         safe_code = clean_code_block(content)
-        
-        # 打印清洗后的 SQL 语句（如果需要查看清洗后的 SQL） 
-        print(f"清洗后的 SQL 查询语句：\n{safe_code}")
+ 
+        print(f"SQL after cleaning:\n{safe_code}")
 
-        
-        # 执行 MySQL 代码
         print("Connecting to the database...")
         connection = pymysql.connect(**db_connection)
         print("Connection successful!")
         cursor = connection.cursor()
         cursor.execute(safe_code)
-        connection.commit()  # 提交事务
+        connection.commit()
 
         if cursor.description is not None: 
             fetched_result = cursor.fetchall()
@@ -458,11 +432,6 @@ def natural_language_query(request):
 
         connection.close()
  
-            # 存储用户查询和 LLM 返回的结果
-        # query_history = UserQueryHistory(user=request.user, query_text=user_input, llm_response=str(fetched_result))
-        # query_history.save()
-
-            # 返回查询结果
         if safe_code.strip().lower().startswith("insert"):
             return JsonResponse({"message": "Data insert successful"})
         elif safe_code.strip().lower().startswith("update"):
